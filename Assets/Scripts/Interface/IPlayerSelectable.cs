1484f7fc93e481569c79740ac1f0b19fb3c2a901
Ken Enzaki <ken11514game@gmail.com>
2025-06-27 23:20:30 +0900

    SCRUM-88 Interface拡張
    Interfaceの拡張とそれを使用するためのベースクラスをタイプごとに作成しました。

--- Assets/Scripts/Interface/IPlayerSelectable.cs
+++ Assets/Scripts/Interface/IPlayerSelectable.cs
@@ -1,4 +1,17 @@
+using UnityEngine;
+
 public interface IPlayerSelectable
 {
-	bool IsSelected();
+	// 選択先のタイプ
+	public enum SelectType{
+		NONE, // あるだけ
+		GETTABLE, // 取得可能オブジェクト
+		ENEMY // 敵
+	}
+
+    bool IsSelected();
+
+    SelectType GetSelectType();
+
+	Vector3 GetBoundsSize();
 }
