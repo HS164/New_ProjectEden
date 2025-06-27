7468912bc9de2692e3bd58c164e3be78cc6d3e3a
Ken Enzaki <ken11514game@gmail.com>
2025-06-27 23:20:30 +0900

    Interface拡張
    Interfaceの拡張とそれを使用するためのベースクラスをタイプごとに作成しました。

---
+++ Assets/Scripts/test_folder/GettableObjectBase.cs
@@ -0,0 +1,51 @@
+using DocumentFormat.OpenXml.Math;
+using UnityEngine;
+
+// 取得可能なオブジェクトのベースクラス
+// これを継承させる
+public class GettableObjectBase : MonoBehaviour , IPlayerSelectable
+{
+    private static readonly string TAG = "GettableObjectBase >> ";
+
+    protected IPlayerSelectable.SelectType selectType = IPlayerSelectable.SelectType.NONE;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected void Start()
+    {
+        Debug.Log(TAG + "Start()");
+        selectType = IPlayerSelectable.SelectType.GETTABLE;
+    }
+
+    // インターフェースメソッド
+    // 選択可能かどうかを返すメソッド
+    public bool IsSelected()
+    {
+        Debug.Log(TAG + "IsSelected()");
+        // 戻り値は動的に変えれるようにする？
+        return true;
+    }
+
+    // インターフェースメソッド
+    // 自分のタイプを返却
+    public IPlayerSelectable.SelectType GetSelectType()
+    {
+        Debug.Log(TAG + "GetSelectType(). selectType : " + selectType);
+        return selectType;
+    }
+
+    // インターフェースメソッド
+    // 自分の幅、高さのサイズを取得
+    public Vector3 GetBoundsSize()
+    {
+        var renderer = GetComponent<Renderer>();
+        Vector3 boundsSize = transform.localScale;
+
+        if (renderer != null)
+        {
+            boundsSize = renderer.bounds.size;
+        }
+
+        Debug.Log(TAG + "boundsSize : " + boundsSize);
+        return boundsSize;
+    }
+}
