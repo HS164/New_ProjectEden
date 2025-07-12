ffa7278489e192bd2072f7c3f2666c26a4d55421
HS164 <r79164@gmail.com>
2025-07-12 17:57:05 +0900

    SCRUM-90 タブ文字からソフトタブ(スペース４)に変更

--- Assets/Scripts/Interface/ICharactorAccessor.cs
+++ Assets/Scripts/Interface/ICharactorAccessor.cs
@@ -1,4 +1,4 @@
 ﻿public interface ICharactorAccessor : IHpAccessor
 {
-    
+
 }
--- Assets/Scripts/Interface/IHelthPointAccessor.cs
+++ Assets/Scripts/Interface/IHelthPointAccessor.cs
@@ -1,14 +1,14 @@
 ﻿public interface IHpAccessor
 {
-	public int CurrentHp
-	{
-		get;
-		protected set;
-	}
+    public int CurrentHp
+    {
+        get;
+        protected set;
+    }
 
-	public int MaxHp
-	{
-		get; 
-		protected set; 
-	}
+    public int MaxHp
+    {
+        get;
+        protected set;
+    }
 }
