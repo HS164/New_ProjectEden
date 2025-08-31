68f5829dfe7a2a2d4d465dce3a097f0f45e54317
Jesse Keene <dovakeen@outlook.com>
2025-08-31 12:30:08 +0900

    SCRUM-66 エネミー生成

---
+++ Assets/Scripts/Enemy/Debug/EnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] Vector3 spawnPosition;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
