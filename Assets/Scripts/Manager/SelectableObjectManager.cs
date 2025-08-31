4b6f49e9114d2e7b7ba375b2e8bc39dc64ff543a
Ken Enzaki <ken11514game@gmail.com>
2025-08-31 12:48:22 +0900

    SCRUM-73 ギミック オブジェクトの選択先の変更

---
+++ Assets/Scripts/Manager/SelectableObjectManager.cs
@@ -0,0 +1,166 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SelectableObjectManager : MonoBehaviour
+{
+    [SerializeField] private Transform centerObject;
+    [SerializeField] private Transform cameraObject;
+
+    // シングルトンインスタンス
+    public static SelectableObjectManager instance { get; private set; }
+
+    private Transform targetObject = null;
+    // 前回の選択可能オブジェクトを保持
+    private IPlayerSelectable beforeWatched = null;
+    // 前回選択したオブジェクトを保持
+    private IPlayerSelectable beforeSelected = null;
+
+    private void Awake()
+    {
+        // インスタンスがまだ存在しない場合、このインスタンスをシングルトンにする
+        if (instance == null)
+        {
+            instance = this;
+            // シーンを切り替えてもこのオブジェクトを破棄しないようにする
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // 既にインスタンスが存在する場合、このオブジェクトを破棄する
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (centerObject == null)
+        {
+            Debug.LogError("centerObject is null");
+        }
+        if (cameraObject == null)
+        {
+            Debug.LogError("cameraObject is null");
+        }
+        targetObject = SearchCenterTarget();
+    }
+
+    public Transform GetTargetObject()
+    {
+        return targetObject;
+    }
+
+    // カメラ上のオブジェクトを選択するメソッド
+    private Transform SearchCenterTarget()
+    {
+        float search_radius = 10f;
+        // center指定した大正からSphereCastでhitしたものを取得
+        var hits = Physics.SphereCastAll(
+            centerObject.transform.position,
+            search_radius,
+            cameraObject.transform.forward,
+            0.01f
+        ).Select(hit => hit.transform).ToList();
+
+        // そのなかの対象のものだけを再取得
+        hits = FilterTargetObject(hits);
+
+        // 対象オブジェクトがなければ、元に戻して終了
+        if (hits.Count <= 0)
+        {
+            if (beforeWatched != null)
+            {
+                beforeWatched.RegisterelSectionCandidate(false);
+                beforeWatched = null;
+            }
+            return null;
+        }
+
+        float minTergetDistance = float.MaxValue;
+        Transform target = null;
+        // 複数hitしたオブジェクトのうち繰り返し処理で近いものを探す
+        foreach (var hit in hits)
+        {
+            Vector3 targetScreenPos = Camera.main.WorldToViewportPoint(hit.position);
+            float targetDistance = Vector2.Distance(new Vector2(0.5f, 0.5f),
+                new Vector2(targetScreenPos.x, targetScreenPos.y));
+            if (targetDistance < minTergetDistance)
+            {
+                minTergetDistance = targetDistance;
+                target = hit.transform;
+            }
+        }
+        Debug.Log(target.name);
+
+        // 現在見ているものを設定する
+        var currentWatch = target.GetComponent<IPlayerSelectable>();
+        // 現在見ているオブジェクトに対して処理を行う
+        SelectionPrediction(currentWatch);
+
+        return target;
+    }
+
+    private List<Transform> FilterTargetObject(List<Transform> hits)
+    {
+        return hits
+        .Where(hit =>
+        {
+            Vector3 screenPoint = Camera.main.WorldToViewportPoint(hit.position);
+            return screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        })
+        .Where(hit => hit.GetComponent<IPlayerSelectable>() != null)
+        .Where(hit => !hit.GetComponent<IPlayerSelectable>().IsSelect)
+        .ToList();
+    }
+
+    // 選択可能なオブジェクトに対して行う処理
+    private void SelectionPrediction(IPlayerSelectable nowWatch)
+    {
+        if (beforeWatched != null && beforeWatched != nowWatch)
+        {
+            // 前回のオブジェクトと今回のオブジェクトが違う場合
+            // 選択候補になっているオブジェクトを入れ替え
+            beforeWatched.RegisterelSectionCandidate(false);
+            nowWatch.RegisterelSectionCandidate(true);
+            beforeWatched = nowWatch;
+        }
+        else if (beforeWatched == null)
+        {
+            // 新しくオブジェクトをセット
+            nowWatch.RegisterelSectionCandidate(true);
+            beforeWatched = nowWatch;
+        }
+    }
+
+    // 渡されたターゲットの選択時処理を行う
+    public IPlayerSelectable SelectTarget(Transform target)
+    {
+        if (target == null)
+        {
+            Debug.Log("target is null");
+            return null;
+        }
+
+        var nowSelect = target.GetComponent<IPlayerSelectable>();
+        if (nowSelect != null && !nowSelect.IsSelect)
+        {
+            nowSelect.OnSelect();
+            ReleaceTarget();
+            beforeSelected = nowSelect;
+        }
+        return nowSelect;
+    }
+
+    // 前回選択時のものを解除する
+    public void ReleaceTarget()
+    {
+        if(beforeSelected == null)
+        {
+            Debug.Log("beforeSelected is null");
+            return;
+        }
+
+        beforeSelected.OnRelease();
+        beforeSelected = null;
+    }
+}
--- Assets/Scripts/SelectableBase/SelectableEnemyBase.cs
+++ Assets/Scripts/SelectableBase/SelectableEnemyBase.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SelectableEnemyBase : MonoBehaviour , IPlayerSelectable
+public abstract class SelectableEnemyBase : MonoBehaviour , IPlayerSelectable
 {
     protected IPlayerSelectable.SelectType selectType = IPlayerSelectable.SelectType.NONE;
 
@@ -9,12 +9,12 @@ public class SelectableEnemyBase : MonoBehaviour , IPlayerSelectable
         selectType = IPlayerSelectable.SelectType.ENEMY;
     }
 
-    // インターフェースメソッド
-    // 選択可能かどうかを返すメソッド
-    public bool IsSelected()
+    /**
+     * 選択候補になっているかどうか
+     */
+    public virtual void RegisterelSectionCandidate(bool selecttion)
     {
-        // 戻り値は動的に変えれるようにする？
-        return true;
+
     }
 
     // インターフェースメソッド
@@ -26,7 +26,7 @@ public class SelectableEnemyBase : MonoBehaviour , IPlayerSelectable
 
     // インターフェースメソッド
     // 自分の幅、高さのサイズを取得
-    public Vector3 GetBoundsSize()
+    public virtual Vector3 GetBoundsSize()
     {
         var renderer = GetComponent<Renderer>();
         Vector3 boundsSize = transform.localScale;
@@ -37,5 +37,33 @@ public class SelectableEnemyBase : MonoBehaviour , IPlayerSelectable
         }
 
         return boundsSize;
+    }
+
+    // インターフェースメソッド
+    // 選択中かどうかの判定
+    public bool IsSelect { get; set; }
+
+    // インターフェースメソッド
+    // 選択時に行う処理
+    public virtual void OnSelect()
+    {
+        if (IsSelect)
+        {
+            // すでに選択時の処理を行っていた場合即時終了
+            return;
+        }
+        IsSelect = true;
+    }
+
+    // インターフェースメソッド
+    // 選択を解除したときに行う処理
+    public virtual void OnRelease()
+    {
+        if (!IsSelect)
+        {
+            // すでに解除時の処理を行っていた場合即時終了
+            return;
+        }
+        IsSelect = false;
     }
 }
