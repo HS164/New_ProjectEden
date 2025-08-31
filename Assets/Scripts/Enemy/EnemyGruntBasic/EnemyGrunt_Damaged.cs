f169e74e0506b24585ef2135e6e5ea77a9a7c0a5
Jesse Keene <dovakeen@outlook.com>
2025-08-31 12:32:09 +0900

    SCRUM-68 ダメージ計算

---
+++ Assets/Scripts/Enemy/EnemyGruntBasic/EnemyGrunt_Damaged.cs
@@ -0,0 +1,16 @@
+﻿using IceMilkTea.Core;
+using UnityEngine;
+
+public partial class EnemyGrunt
+{
+    /// <summary>
+    ///　攻撃れた時のステート
+    /// </summary>
+    private class EnemyGrunt_Damaged : ImtStateMachine<EnemyGrunt>.State
+    {
+        protected override void Enter()
+        {
+            Context.navAgent.isStopped = true;
+        }
+    }
+}
