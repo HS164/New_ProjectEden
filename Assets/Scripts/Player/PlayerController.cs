ce3d7e6d69399120a81dcf8b785283251e47e7e9
Jesse Keene <dovakeen@outlook.com>
2025-10-26 17:25:29 +0900

    SCRUM-133 コメントの削除

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -187,7 +187,6 @@ public partial class PlayerController : MonoBehaviour, IDamageable, IPlayer
     {
         var moveAmt = input.Move.ReadValue<Vector2>();
         var inputDir = new Vector3(moveAmt.x, 0, moveAmt.y);
-        // カメラと同期させてはいけない　させると移動中にカメラを動かすとカクカクになる
         var dir = Quaternion.Euler(0, cameraObj.transform.eulerAngles.y, 0);
         var moveDirection = dir * inputDir;
         /****              ここまで                ****/
